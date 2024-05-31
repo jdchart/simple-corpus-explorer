26f269d3c478916e008f9ec1c3be09aaefebd6f5
Jacob Hart <>
2024-05-31 05:46:48 +0200

    another base

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     kit: {
         adapter: adapter(),
 		paths : {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages/simple-corpus-explorer' : '',
+			base: process.env.NODE_ENV === 'production' ? '/simple-corpus-explorer' : '',
 		}
     }
 };
